ec47ec667dfd5fe86987333744fb5ef933120e88
Jamal Bishop <rosalindanegrin@Rosalindas-MacBook-Air.local>
2023-06-06 10:57:33 -0400

    Testing commit

---
+++ challenges/main.tf
@@ -0,0 +1,51 @@
+resource "kubernetes_service" "webapp-service" {
+  metadata {
+    name = "webapp-service"
+  }
+  spec {
+    port {
+      node_port   = 30080
+      target_port = 8080
+    }
+
+    type = "NodePort"
+  }
+}
+
+resource "kubernetes_deployment" "frontend" {
+    metadata {
+    name = "frontend"
+    labels = {
+      name = "frontend"
+    }
+  }
+
+  spec {
+    replicas = 4
+
+    selector {
+      match_labels = {
+        name = "webapp"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          test = "MyExampleApp"
+        }
+      }
+
+      spec {
+        container {
+          image = "kodekloud/webapp-color:v1"
+          name  = "simple-webapp"
+          port {
+            container_port = 8080
+          }
+        }
+      }
+    }
+  }
+  
+}
